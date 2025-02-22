f781ecde6073c5cb411f609b68c90b7977df2e3e
Alexander Fedorako <fedorakooo@gmail.com>
2025-02-22 18:31:19 +0300

    Add loop_modify.py

---
+++ src/loop_modify.py
@@ -0,0 +1,21 @@
+def modify_loop_counts(operators):
+    # do ... while
+    if operators.get('do'):
+        count_do = operators['do']
+        operators['while'] -= count_do
+        if count_do:
+            operators['do ... while'] = count_do
+
+    # for ... of
+    if operators.get('of'):
+        count_of = operators['of']
+        operators['for'] -= count_of
+        if count_of:
+            operators['for ... of'] = count_of
+
+    # for ... in
+    if operators.get('in'):
+        count_in = operators['in']
+        operators['for'] -= count_in
+        if count_in:
+            operators['for ... in'] = count_in
