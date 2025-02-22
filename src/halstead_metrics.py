1d49e34c5cf57ca7cdde160123932812f6751093
Alexander Fedorako <fedorakooo@gmail.com>
2025-02-22 18:31:30 +0300

    Update halstead_metrics.py

--- src/halstead_metrics.py
+++ src/halstead_metrics.py
@@ -1,16 +1,19 @@
 import math
+import pprint
+from collections import Counter
+from typing import OrderedDict
 
 from pygments import lex
 from pygments.lexers import JavascriptLexer
 from pygments.token import Token, is_token_subtype
 
-operators = []
-operands = []
-operators_dictionary = {}
-operands_dictionary = {}
+from loop_modify import modify_loop_counts
+
 
 def calculate_halstead_metrics(code: str) -> dict:
     """Вычисляет метрики Холстеда для заданного JavaScript-кода."""
+    operators = []
+    operands = []
 
     try:
         lexer = JavascriptLexer()
@@ -34,9 +37,20 @@ def calculate_halstead_metrics(code: str) -> dict:
               is_token_subtype(token_type, Token.Literal.Number)):
             operands.append(token_value.strip())
 
+    operators_count = Counter(operators)
+    operands_count = Counter(operands)
+
+    operators_dict = dict(operators_count)
+    operands_dict = dict(operands_count)
+
+    modify_loop_counts(operators_dict)
+
+    print(operands_dict)
+    print(operators_dict)
+
     # Рассчитываем уникальные и общие количества
-    n1 = len(set(operators)) if operators else 0
-    n2 = len(set(operands)) if operands else 0
+    n1 = len(OrderedDict.fromkeys(operators).keys()) if operators else 0
+    n2 = len(OrderedDict.fromkeys(operands).keys()) if operands else 0
     N1 = len(operators)
     N2 = len(operands)
 
