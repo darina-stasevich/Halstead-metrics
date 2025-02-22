9e7814eb1b67f3b6ce7288753cfb43b5b8013adc
Alexander Fedorako <fedorakooo@gmail.com>
2025-02-22 18:31:06 +0300

    Update code.js

--- js/code.js
+++ js/code.js
@@ -9,3 +9,9 @@ function is_odd(num) {
 for (var i = 1; i < 100; i++) {
     document.write(i + ": " + is_odd(i) + "<br>");
 }
+
+
+const arr = [1, 2, 3];
+for (let value of arr) {
+    console.log(value);
+}
